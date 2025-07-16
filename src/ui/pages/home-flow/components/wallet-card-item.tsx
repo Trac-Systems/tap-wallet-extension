7b51396d0cd1a898def7fbb353d02d2bc21868cd
taipv-angelhack <tai.pham@angelhack.com>
2025-07-16 16:45:35 +0700

    Update UI and optimize performance for WalletCard: only fetch USD price for active card

--- src/ui/pages/home-flow/components/wallet-card-item.tsx
+++ src/ui/pages/home-flow/components/wallet-card-item.tsx
@@ -53,6 +53,7 @@ const WalletCard = (props: IWalletCardProps) => {
   }, [activeWallet]);
 
   const {address} = activeAccount;
+  const isActive = keyring.key === activeWallet.key;
 
   const balanceValue = useMemo(() => {
     return satoshisToAmount(Number(accountBalance.amount ?? 0));
@@ -77,8 +78,9 @@ const WalletCard = (props: IWalletCardProps) => {
   );
 
   useEffect(() => {
+    if (!isActive) return;
     debouncedFetchDataUSD(balanceValue, setUsdPrice);
-  }, [balanceValue, debouncedFetchDataUSD]);
+  }, [balanceValue, debouncedFetchDataUSD, isActive]);
 
   useEffect(() => {
     return () => {
@@ -105,31 +107,9 @@ const WalletCard = (props: IWalletCardProps) => {
   }, [menuOpen]);
 
   useEffect(() => {
+    if (!isActive) return;
     wallet.getUSDPrice(Number(safeBalance)).then(setUsdAvailable);
-  }, [safeBalance]);
-
-  useEffect(() => {
-    if (debounceTimeoutRef.current) {
-      clearTimeout(debounceTimeoutRef.current);
-    }
-    
-    debounceTimeoutRef.current = setTimeout(async () => {
-      if (activeAccount?.address) {
-        setSafeBalanceLoading(true);
-        try {
-          await fetchUtxos();
-        } finally {
-          setSafeBalanceLoading(false);
-        }
-      }
-    }, 200);
-
-    return () => {
-      if (debounceTimeoutRef.current) {
-        clearTimeout(debounceTimeoutRef.current);
-      }
-    };
-  }, [activeAccount?.address, fetchUtxos]);
+  }, [safeBalance, isActive]);
 
   
   //! Render
@@ -169,12 +149,13 @@ const WalletCard = (props: IWalletCardProps) => {
             )}
           </UX.Box>
         </div>
+        <div>
         {checkIsSingleWallet ? null : (
           <UX.Box
             layout="row"
             spacing="xl"
             onClick={handleOpenDrawerAccount}
-            style={{cursor: 'pointer'}}>
+            style={{cursor: 'pointer', marginBottom: 4}}>
             <UX.Text
               styleType="heading_16"
               customStyles={{
@@ -192,16 +173,18 @@ const WalletCard = (props: IWalletCardProps) => {
         )}
 
         <UX.AddressBar address={address} />
+        </div>
         <UX.Box layout="row_between">
           <UX.Box layout="row" spacing="xss_s">
             <UX.Tooltip text={balanceValue} isText>
               <UX.Text
                 styleType="heading_20"
-                title={balanceValue}
+                title={Number(balanceValue).toFixed(8)}
                 className="textBalance"
+                customStyles={{lineHeight: '32px', fontSize: 24}}
               />
             </UX.Tooltip>
-            <UX.Text styleType="heading_20" title={'BTC'} />
+            <UX.Text styleType="heading_20" customStyles={{lineHeight: '32px', fontSize: 24}} title={'BTC'} />
           </UX.Box>
           <UX.Box layout="row" spacing="xss_s">
             <UX.Text title="≈" styleType="body_16_normal" />
@@ -216,7 +199,7 @@ const WalletCard = (props: IWalletCardProps) => {
           </UX.Box>
         </UX.Box>
         <UX.Box>
-          <UX.Text styleType="body_14_normal" title="Available balance:"  />
+          <UX.Text styleType="body_12_normal" title="Available balance:"  />
           <UX.Box layout="row_between">
             <UX.Box layout="row" spacing="xss_s">
               {safeBalanceLoading ? (
@@ -254,24 +237,6 @@ const WalletCard = (props: IWalletCardProps) => {
         </UX.Box>
       </div>
 
-      <div className="groupAction">
-          <div className="groupBox" onClick={() => navigate('/home/send')}>
-            <SVG.ArrowSendIcon />
-            <UX.Text
-              title="Send"
-              styleType="body_14_bold"
-              customStyles={{color: 'white'}}
-            />
-          </div>
-          <div className="groupBox" onClick={() => navigate('/home/receive')}>
-            <SVG.ArrowReceiveIcon />
-            <UX.Text
-              title="Receive"
-              styleType="body_14_bold"
-              customStyles={{color: 'white'}}
-            />
-          </div>
-        </div>
     </>
   );
 };
